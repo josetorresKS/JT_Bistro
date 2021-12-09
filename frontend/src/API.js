82daffcc86eca5031a0f077a1a4eb6d8a515a8e4
josetorresKS <josemt0016@gmail.com>
2021-12-09 01:23:51 -0800

    bistro2

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -4,9 +4,9 @@ var baseURL;
 if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === "PRODUCTION") {
     baseURL = process.env.REACT_APP_API_BASE_URL;
 } else {
-    baseURL = "http://127.0.0.1:8000";
+    baseURL = "https://jtbistro-backend.herokuapp.com/";
 }
-// baseURL = "https://backend-akshay.herokuapp.com/";
+
 
 const api = axios.create({
   baseURL: baseURL,
